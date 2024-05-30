eec986d1f15aead249d8b65f4daf8686830e6ca0
Sabrina Godoy <sabrygodoy1@gmail.com>
2024-05-30 20:53:15 -0300

    agregando express primeros pasos

--- clases/proyecto/server.js
+++ clases/proyecto/server.js
@@ -1,11 +1,26 @@
-const http = require("http");
+// HTTP server
+
+//const http = require("http");
+
+// const PORT = 8080;
+// const HOST = "127.0.0.1";
+// const server = http.createServer((request, response) => {
+//     response.setHeader("Content-Type", "text/plain; charset=utf8");
+//     response.end("¡Mi primer hola mundo desde el backend!");
+// });
+
+// server.listen(PORT, () => {
+//     console.log(`Ejecutandose en http://${HOST}:${PORT}`);
+// });
+
+import express from "express";
 
 const PORT = 8080;
 const HOST = "127.0.0.1";
-
-const server = http.createServer((request, response) => {
-    response.setHeader("Content-Type", "text/plain; charset=utf8");
-    response.end("¡Mi primer hola mundo desde el backend!");
+const server = express();
+//Endpoint
+server.get("/saludo", ( req, res) => {
+    res.send("¡Hola a todos, pero ahora desde express!");
 });
 
 server.listen(PORT, () => {
