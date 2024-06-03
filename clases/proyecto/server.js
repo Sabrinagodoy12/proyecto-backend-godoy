c177e4e4ba51c42e52ad645679462cf85aca534d
Sabrina Godoy <sabrygodoy1@gmail.com>
2024-06-03 17:27:02 -0300

    api

--- clases/proyecto/server.js
+++ clases/proyecto/server.js
@@ -5,24 +5,48 @@ const server = express();
 const PORT = 8080;
 const HOST = "localhost";
 
-server.get("/", ( req, res) => {
+server.use(express.urlencoded({extended: true}));
+server.use(express.json());
+
+server.get("/api/usuarios", ( req, res) => {
     const {genero} = req.query;
     if(genero) {
-        const usuario = usuarios.find((item) => item.genero === genero?.trim().toUpperCase());
-       
+        const usuario = usuarios.filter((item) => item.genero === genero.trim().toUpperCase());
+
         if(!usuario){
-            return res.send({"Error": "no se encontró ningún usuario"})
+            return res.status(400).send({"Error": "no se encontró ningún usuario"})
+    }
+    return res.status(200).send(JSON.stringify(usuarios));
     }
     res.send(JSON.stringify(usuarios));
-    }
 });
 
-// server.get("/:userId", ( req, res) => {
-//     const {userId} = req.params;
-//     const usuario = usuarios.find((item) => item.id === Number(userId));
+server.get("/api/usuarios/:id", ( req, res) => {
+    const { id } = req.params;
+    const usuario = usuarios.find((item) => item.id === Number(id));
 
-//     console.log(usuario);
-//     res.send(usuario);
+        if(!usuario){
+            return res.status(400).send({"Error": "no se encontró ningún usuario"});
+    }
+   res.status(200).send({data: usuario});
+    }
+);
+
+server.get("/api/usuarios", ( req, res) => {
+    const { id, nombre, apellido, edad, correo, genero } = req.body;
+
+        if(!nombre || !apellido || !edad || !correo || !genero){
+            return res.status(400).send({"Error": "Faltan datos"});
+    }
+    const usuarioNuevo = { id, nombre, apellido, edad, correo, genero};
+    usuario.push(usuarioNuevo);
+
+   res.status(200).send({data: usuarioNuevo});
+    }
+);
+
+// server.update("/", ()=>{
+
 // });
 
 server.listen(PORT, () => {
