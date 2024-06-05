b0665b3305980606b3e424c8a7dafa5130a9115f
Sabrina Godoy <sabrygodoy1@gmail.com>
2024-06-05 00:08:54 -0300

    id automatico

--- src/products.js
+++ src/products.js
@@ -1,6 +1,44 @@
+// class productsManager {
+//     static products = [];
+//     #crypto;
+//     #algorithn;
+//     #key;
+//     #vi;
+
+//     constructor(){
+//         this.#crypto = require("crypto");
+//         this.#algorithn = "AES-128-CBC";
+//         this.#key = "misProductoSelec";
+//         this.#vi = this.#crypto.randomBytes(16);
+//     }
+//     encriptar = (texto) => {
+//         const cifrador = this.#crypto.createCipheriv(this.#algorithn, this.#key, this.#vi);
+
+//         let textoEncriptado = cifrador.update(texto, "utf8", "hex");
+//         textoEncriptado = cifrador.final("hex");
+
+//         return textoEncriptado
+//     }
+// }
+const generateId = () => {
+    const number = Math.floor(Math.random()* 100000);
+    const albs = [...'abcdefghijklmnopqrstuvwxyz'];
+    let stringPart = '';
+
+    for( let i=0; i<10; i++){
+        const index = randomIndex(25);
+        stringPart += albs[index]
+    }
+    const id = stringPart+number;
+    return id;
+}
+const randomIndex = (maxNumber) => {
+    return Math.floor(Math.random()*maxNumber)
+}
+
 const products = [
     {
-        id:1,
+        id: generateId(),
         title: "Taza rosa",
         description: "Taza de cerámica brillante",
         price: 3400,
@@ -9,7 +47,7 @@ const products = [
         category: "Tazas",
     },
     {
-        id:2,
+        id:generateId(),
         title: "Taza con dibujo",
         description: "Taza de cerámica con dibujo impreso",
         price: 3400,
@@ -18,7 +56,7 @@ const products = [
         category: "Tazas",
     },
     {
-        id:3,
+        id:generateId(),
         title: "Bowl",
         description: "Bowl de cerámica tamaño grande",
         price: 4500,
@@ -27,7 +65,7 @@ const products = [
         category: "Bowls",
     },
     {
-        id:4,
+        id:generateId(),
         title: "Plato marrón",
         description: "Plato de cerámica playo grande",
         price: 6000,
