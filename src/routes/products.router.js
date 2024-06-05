d4f09341356cdcd6743f8999f5cfbeed3af90bb4
Sabrina Godoy <sabrygodoy1@gmail.com>
2024-06-04 21:24:33 -0300

    carpeta public

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -7,23 +7,35 @@ router.get("/", (req, res) => {
     res.status(200).send({state: "success", data: products});
 });
 
-// server.get("/api/products/:id", ( req, res) => {
-//     const { id } = req.params;
-//     const product = products.find((item) => item.id === Number(id));
+router.get("/api/products/:id", ( req, res) => {
+    const { id } = req.params;
+    const product = products.find((item) => item.id === Number(id));
 
-//         if(!product){
-//             return res.status(400).send({"Error": "no se encontró ningún usuario"});
-//     }
-//    res.status(200).send({data: product});
-//     }
-// );
+        if(!product){
+            return res.status(400).send({"Error": "no se encontró ningún producto"});
+    }
+   res.status(200).send({data: product});
+    }
+);
 
 router.post("/", (req, res) => {
     const {id, title, description, code, price, status, stock, category, thumbnails} = req.body;
-    const product = {id, title, description, code, price, status, stock, category, thumbnails}
-    users.push(user);
+    const newProduct = {id: Number(id), title, description, code, price, status, stock, category, thumbnails}
 
-    res.status(201).send({state: "success", data: product });
+    users.push(newProduct);
+
+    res.status(201).redirect("http://localhost:8080/public/") ;
+});
+
+router.delete("/api/products/:id", ( req, res) => {
+    const {id} = req.params;
+    const indice = products.findIndex((item) => item.id === Number(id));
+
+    if(indice < 0){
+        return res.status(400).send({"Error": "no se encontró ningún producto"});
+    }
+    products.splice(indice, 1);
+    res.status(200).send({data: "Producto eliminado"});
 });
 
 export default router;
