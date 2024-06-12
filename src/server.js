ad25b03057042252350a1e8f71693d87758155e8
Sabrina Godoy <sabrygodoy1@gmail.com>
2024-06-12 20:01:31 -0300

    path

--- src/server.js
+++ src/server.js
@@ -1,17 +1,16 @@
 import express from "express";
-import path from "path";
+import path from "./utils/path.js";
 import cartsRouter from "./routes/carts.router.js";
 import productsRouter from "./routes/products.router.js";
-
 
 const PORT = 8080;
 const HOST = "localhost";
 const server = express();
 
-server.use(express.json());
-server.use(express.urlencoded({extended: true}));
+// server.use(express.json());
+// server.use(express.urlencoded({extended: true}));
 
-server.use("/public", express.static(path.join(path.basename("src"), "public")));
+server.use("/api/public", express.static(path.public));
 
 server.use("/api/carts", cartsRouter);
 server.use("/api/products", productsRouter);
